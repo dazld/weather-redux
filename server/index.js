d06b589867b7140da1990827bc5a9bcdac071b7f
Dan Peddle <dan@flarework.com>
2016-03-03 12:16:19 +0100

    sass and server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
-const app = require('express')();
+const es6render = require('express-es6-template-engine');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const { resolve } = require('url');
+const _ = require('lodash');
 
+// const config = require('../assets/js/lib/config').default;
+// const assetPath = require('../assets/js/lib/asset-path').default;
+// const indexHTML = fs.readFileSync(path.resolve(__dirname, '../static/index.html')).toString();
 
-app.get('/foo', function(req,res){
-    res.send('bar');
-});
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+app.engine('html', es6render);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'html');
+
+function serveIndex(req,res) {
+    res.render('index', {
+        locals: {
+            title: 'WeatherThing'
+        }
+    });
+}
+
+app.get('/', serveIndex);
 
 module.exports = app;
